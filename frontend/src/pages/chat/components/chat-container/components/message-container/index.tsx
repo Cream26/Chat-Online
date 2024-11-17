331dc9e5ce219eb455cbe5b41b705595b9d4be4b
Cream <zhangtao4901@gmail.com>
2024-11-17 15:57:07 +0800

    feat: 聊天界面静态搭建

---
+++ frontend/src/pages/chat/components/chat-container/components/message-container/index.tsx
@@ -0,0 +1,7 @@
+const MessageContent = () => {
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[7vw] xl:w-[80vw] w-full"></div>
+  );
+};
+
+export default MessageContent;
---
+++ frontend/src/pages/chat/components/contacts-container/components/profile-info/index.tsx
@@ -0,0 +1,84 @@
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { useAppStore } from "@/store";
+import { getColor } from "@/lib/utils";
+import { HOST } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FiEdit2 } from "react-icons/fi";
+import { IoPowerSharp } from "react-icons/io5";
+const ProfileInfo = () => {
+  const navigate = useNavigate();
+  const { userinfo } = useAppStore();
+  const Logout = async () =>{
+    console.log("Logout");
+  }
+  return (
+    <div className="absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#212b33]">
+      <div className="flex gap-3 items-center justify-center">
+        <div className="w-12 h-12 relative">
+          <Avatar className="w-12 h-12 rounded-full overflow-hidden">
+            {userinfo?.image ? (
+              <AvatarImage
+                src={`${HOST}/${userinfo?.image}`}
+                alt="profile"
+                className="object-cover h-full w-full rounded-full bg-black"
+              />
+            ) : (
+              <div
+                className={`uppercase w-32 h-32 md:w-48 md:h-48 text-lg border-[1px] flex justify-center items-center rounded-full ${getColor(
+                  userinfo?.color ?? 0
+                )} `}
+              >
+                {userinfo?.firstName
+                  ? userinfo?.firstName.split("").shift()
+                  : userinfo?.email?.split("").shift()}
+              </div>
+            )}
+          </Avatar>
+        </div>
+        <div>
+          {userinfo?.firstName && userinfo?.lastName
+            ? `${userinfo.firstName} ${userinfo.lastName}`
+            : ""}
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <FiEdit2
+                className="text-purple-500 text-xl font-medium"
+                onClick={() => {
+                  navigate("/profile");
+                }}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+              Edit Profile
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <IoPowerSharp
+                className="text-red-500 text-xl font-medium"
+                onClick={Logout}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+              Logout
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
